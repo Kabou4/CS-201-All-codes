21136bf411ad3d6215e048edc4c1a253116ca4f0
Kabou4 <abdalmeida4@gmail.com>
2025-10-22 12:00:57 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -6,6 +6,9 @@ import java.util.*;
 
 public class Time {
 	//data fields, every Time class has hour, minute, and second. This is encapsulation!
+	public int year;
+	public int month;
+	public int day;
 	public int hour;
 	public int minute;
 	public int second;
@@ -13,15 +16,56 @@ public class Time {
 	//methods
 	public void input() {
 		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter year: ");
+		year = keyboard.nextInt();
+		System.out.print("Enter month: ");
+		month = keyboard.nextInt();
+		while(month > 12 || month < 0){
+	        System.out.println("This is an invalid entry for month, try again!");
+	        System.out.print("Enter month: ");
+	        month = keyboard.nextInt();
+		}
+		System.out.print("Enter day: ");
+		day = keyboard.nextInt();
 		System.out.print("Enter hour: ");
 		hour = keyboard.nextInt();
+		while(hour > 23 || hour < 0){
+	        System.out.println("This is an invalid entry for hour, try again!");
+	        System.out.print("Enter hour: ");
+	        hour = keyboard.nextInt();
+		}
 		System.out.print("Enter minute: ");
 		minute = keyboard.nextInt();
+		while(minute > 59 || minute < 0){
+	        System.out.println("This is an invalid entry for minute, try again!");
+	        System.out.print("Enter minute: ");
+	        minute = keyboard.nextInt();
+		}
 		System.out.print("Enter seconds: ");
 		second = keyboard.nextInt();
+		while(second > 59 || second < 0){
+	        System.out.println("This is an invalid entry for second, try again!");
+	        System.out.print("Enter second: ");
+	        second = keyboard.nextInt();
+		}
+    }
+	public void PrintStandardTime() {
+		String ampm = "AM";
+		int sHour = hour;
+		if(hour == 12){
+		    ampm = "PM";
+		}
+		else if(hour == 0){
+		    sHour = 12;
+		}
+		else if(hour > 12){
+		   ampm = "PM";
+		   sHour = hour - 12;
+		}
+		System.out.println("Based on your entry, the Standard time is " + month + "-" + day + "-" + year + ", " + sHour + ":" + minute + ":" + second + " " + ampm + ".");
 	}
-
-	public void print() {
-		System.out.println(hour + ":" + minute + ":" + second);
+	
+	public void PrintMilitaryTime() {
+		System.out.println("Based on your entry, the Military time is " + day + "-" + month + "-" + year + ", " + hour + ":" + minute + ":" + second + ".");
 	}
 }
---
+++ programming6.java
@@ -0,0 +1,8 @@
+public class programming6 {
+	public static void main(String[] args) {
+		Time t1 = new Time();
+		t1.input();
+		t1.PrintStandardTime();
+		t1.PrintMilitaryTime();
+	}
+}
